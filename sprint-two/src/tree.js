a30e72ce5b74064a09b8d86b08eea90f71bc707b
maximilian183 <max.kwok@gmail.com>
2017-05-29 09:47:27 -0700

    Sprint 2 - Tree done!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,35 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function(value) {    // Complexity: O(1)
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target) {    // Complexity: O(n^n)
+  var check = false;
+
+  var searchTree = function(tree) {
+    if( tree.value === target) {
+      check = true;
+      return check;
+    } else {
+      for(var i = 0; i < tree.children.length; i++) {
+        searchTree(tree.children[i]);
+      }
+    }
+  }
+
+  searchTree(this);
+  return check;
+
 };
 
 
